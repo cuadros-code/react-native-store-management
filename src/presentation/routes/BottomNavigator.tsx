1a2e4aebf4297bf7012fcee23504a70562bc77d6
kevin cuadros <kevindavidcuadros@gmail.com>
2024-05-27 21:12:23 -0500

    chore: Update BottomNavigator styling for tab icons and labels

--- src/presentation/routes/BottomNavigator.tsx
+++ src/presentation/routes/BottomNavigator.tsx
@@ -10,13 +10,16 @@ const BottomNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen options={{
-        tabBarIcon: ({ color }) => <Text style={{ color }}>Home</Text>
+        tabBarIcon: ({ color }) => <Text style={{ color, fontSize: 16 }}>Home</Text>,
+        tabBarLabel: () => <></>
       }} name="Home" component={HomeScreen} />
       <Tab.Screen options={{
-        tabBarIcon: ({ color }) => <Text style={{ color }}>Profile</Text>
+        tabBarIcon: ({ color }) => <Text style={{ color, fontSize: 16 }}>Profile</Text>,
+        tabBarLabel: () => <></>
       }} name="Profile" component={ProfileScreen} />
       <Tab.Screen options={{
-        tabBarIcon: ({ color }) => <Text style={{ color }}>Settings</Text>
+        tabBarIcon: ({ color }) => <Text style={{ color, fontSize: 16 }}>Settings</Text>,
+        tabBarLabel: () => <></>
       }} name="Settings" component={SettingScreen} />
     </Tab.Navigator>
   )
